551e70b9305560bd29529c220ae4feca23886874
Alisson Gritti <alissongritti@hotmail.com>
2026-03-06 16:52:08 -0300

    ajustando grupoService

--- src/services/grupoService.js
+++ src/services/grupoService.js
@@ -1,13 +1,15 @@
-const db = require("../database");
+const { getDb } = require("../database");
 
 async function salvarDiscord(groupId, link) {
+  const db = getDb();
   const query = `INSERT OR REPLACE INTO grupos (id_grupo, link_discord) VALUES (?, ?)`;
-  await db.run(query, [groupId, link]); // Ajuste o 'db.run' se usar outro método
+  await db.run(query, [groupId, link]);
 }
 
 async function obterDiscord(groupId) {
+  const db = getDb();
   const query = `SELECT link_discord FROM grupos WHERE id_grupo = ?`;
-  const row = await db.get(query, [groupId]); // Ajuste o 'db.get' se usar outro método
+  const row = await db.get(query, [groupId]);
   return row ? row.link_discord : null;
 }
 
